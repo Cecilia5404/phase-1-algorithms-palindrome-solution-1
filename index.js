e1bc482f483d7f244b260acfefcd83b32d161a02
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-06-18 08:28:46 +0300

    pali

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let rightIndex = 0;
+  let leftIndex = word.length - 1;
+
+  while(rightIndex < leftIndex) {
+    if (word [rightIndex] !== word[leftIndex]) {
+      return false;
+}
+rightIndex++;
+    leftIndex--;
+  }
+  return true;
+}
+function pali(word){
+
 }
 
 /* 
@@ -9,10 +23,15 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+ if (require.main === module) {
 
+ }
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
